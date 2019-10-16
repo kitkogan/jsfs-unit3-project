067a9db941dfe92f2154b2b9052dffeac242f91d
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-15 23:59:33 -0500

    Add functionality for "T-shirt info section"

--- interactive_form-v1/js/script.js
+++ interactive_form-v1/js/script.js
@@ -2,15 +2,12 @@
 
 $(function() {
     $( '#name' ).focus();
-  });
+  
 
-  //Hide text input for 'other' element initially if JS is running
-
-
-
+  //Hide text input for 'other' element initially, show textbox when 'other' is selected (change event)
+    
   $(function() {
-    var 
-    title = $('#title'),
+    var title = $('#title'),
     onChange = function(event) {
         if ($(this).val() === 'other') {
             $('#other-title').show();
@@ -19,6 +16,36 @@ $(function() {
             $('#other-title').hide();
         }
     };
-    onChange.apply(title.get(0)); // To show/hide the Other textbox initially
+    onChange.apply(title.get(0));
     title.change(onChange);
 }); 
+
+//Hide color menu so that user can only select valid option combinations for "Design" and "Color" fields 
+//add HTML prompt instructing user to choose a T-shirt theme
+ $('#color').html("<option value='none'>Please select a T-shirt Theme</option>");
+ var themeSelected = false;
+$( "#design").change(function() {
+  // if "Theme- JS Puns" is selected show relevant color options
+      if ($("#design option:selected").text() == "Theme - JS Puns") {
+          $('#colors-js-puns').show();
+          $("#color").html("<option value='cornflowerblue'>Cornflower Blue</option><option value='darkslategrey'>Dark Slate Grey</option><option value='gold'>Gold</option>");
+          themeSelected = true;
+          return themeSelected
+          }
+          // if "Theme - I ♥ JS" has been selected show relevant color options
+      else if ($("#design option:selected").text() == 'Theme - I ♥ JS') {
+              $('#colors-js-puns').show();
+              $("#color").html("<option value='tomato'>Tomato</option><option value='steelblue'>Steel Blue</option><option value='dimgrey'>Dim Grey</option>");
+              themeSelected = true;
+              return themeSelected;
+          }
+          // if user has not made a selection show HTML prompt instructing user to choose a T-shirt theme
+      else {
+              $('#colors-js-puns').show();
+              $('#color').html("<option value='none'>Please select a T-shirt Theme</option>");
+              themeSelected = true;
+              return $('#color').html("<option value='none'>Please select a T-shirt Theme</option>");
+      }
+});
+
+});
