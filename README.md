202ce8136fa2f5b306d389d367a63fb675823136
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-17 17:31:12 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # jsfs-unit3-project
- Form Validation project
+ Interactive Form project
+
+ This project focused on many of the aspects of adding functionality to web forms by using jQuery methods to enhance the supplied HTML. Students were also asked to use unobtrusive JavaScript and progressive enhancement to ensure that the critical form elements could be accessed across browsers and in instances where JavaScript was disabled or otherwise not able to run.
+
+A major component of this project involved the implementatiion of form validation, which includes use of loops, conditional commands, and event handlers to determine whether validation criteria is met.
+
+Some other tasks for this project included use of various jQuery methods to add elements and traverse the DOM as well as showing/hiding form elements based on user selection from a chechbox or menu. This also included real time cost totaling when user makes a selection from Activities list and functionality to allow user to only select activities that do not conflict with eachother.
+
+Some additional programming I chose to include for extra credit:
+-Hiding the color label and select menu until user selection is made
+-Generating conditional form validation error messages based on whether the input is too short/long (credit card), with additional error message if field left blank
+-Real time error message displayed as user begins to type in email text field (rather than only validating on form submit)
--- interactive_form-v1/js/script.js
+++ interactive_form-v1/js/script.js
@@ -1,10 +1,11 @@
+/////////Treehouse JSFS Unit 3 Project- Interactive Form///////////////
+
 //Set focus to first input field ("Name") at page load
 
 $(function() {
   $( '#name' ).focus();
   
-
-  //Hide text input for 'other' element initially, show textbox when 'other' is selected (change event)
+ //Hide text input for 'other' element initially, show textbox when 'other' is selected (change event)
     
   $(function() {
     var title = $('#title'),
@@ -22,9 +23,9 @@ $(function() {
 
 ////////T-shirt section//////////   
 
-//Hide color menu until theme is chosen
-//Only allow user to view and select correct color options based on theme choice
-//Add un-selectable HTML prompt instructing user to choose a T-shirt theme if none is selected after color menu is revealed
+//Hide color label and select menu until theme is chosen
+//Only allow user to view and select correct color options based on theme choice, starting with 1st option from each respective list
+//Add un-selectable menu prompt instructing user to choose a T-shirt theme if none is selected, after color menu is revealed
 
 $("#colors-js-puns").hide();
 $('#design').change(function(event){
@@ -48,21 +49,27 @@ $('#design').change(function(event){
   $('#color').html("<option value='none'>Please select a T-shirt Theme</option>");
     var themeSelected = false;
     $( "#design").change(function() {
+
   // if "Theme- JS Puns" is selected show relevant color options
+
     if ($("#design option:selected").text() == "Theme - JS Puns") {
           $('#colors-js-puns').show();
           $("#color").html("<option value='cornflowerblue'>Cornflower Blue</option><option value='darkslategrey'>Dark Slate Grey</option><option value='gold'>Gold</option>");
           themeSelected = true;
           return themeSelected
           }
+
           // if "Theme - I ♥ JS" has been selected show relevant color options
+
       else if ($("#design option:selected").text() == 'Theme - I ♥ JS') {
               $('#colors-js-puns').show();
               $("#color").html("<option value='tomato'>Tomato</option><option value='steelblue'>Steel Blue</option><option value='dimgrey'>Dim Grey</option>");
               themeSelected = true;
               return themeSelected;
           }
+
           // if user has not made a selection show HTML prompt instructing user to choose a T-shirt theme
+
       else {
               $('#colors-js-puns').show();
               $('#color').html("<option value='none'>Please select a T-shirt Theme</option>");
@@ -72,13 +79,16 @@ $('#design').change(function(event){
     });
 
 //////////Activities section/////////////
-//create new DOM element to display total cost and append it to ' .activities' section
+
+ //Create new DOM element to display total cost and append it to ' .activities' section
+
     let totalCostCalc = 0;
     var totalCostCalcElem = $('<span></span>').html('<b>Total: $' + totalCostCalc + '</b>');
 
     $('.activities').append(totalCostCalcElem);
  
  //Update and display the total cost of the activities selected
+
     $('.activities').change(function(event) {
         const checked = event.target;
         let cost = parseInt($(checked).attr('data-cost').replace(/\D(\d+)/, '$1'));
@@ -90,7 +100,8 @@ $('#design').change(function(event){
      totalCostCalcElem.html('<b>Total: $' + totalCostCalc + '</b>');
     }
 
- //Don't allow user to select conflicting activities
+  //Don't allow user to select conflicting activities
+
         var activitiesList = $('.activities input');
 
         $(activitiesList).each(function(index) {
@@ -107,10 +118,12 @@ $('#design').change(function(event){
 
 /////////Payment Info/////////
 
- //Display payment sections based on the paymeny option chosen from select menu
- //CC payment option shown by default and hide PP and BC options
+ //Display payment sections based on the payment option chosen from select menu
+ //CC payment option shown by default, hide PP and BC options
 
- //'Select Payment Method' should be hidden from dropdown, credit Card option is default selection
+ //'Select Payment Method' should be hidden from dropdown
+ //Credit Card option is default selection
+
     $('option[value="select method"]').attr('hidden', true);
 
     $('option[value="Credit Card"]').attr('selected', true);
@@ -141,11 +154,10 @@ $('#design').change(function(event){
     });
 
 
-
 /////////Form Validation//////////
   
- //Name field must contain at leasst one letter
-
+ //Name field must contain at leasst one letter, 
+ //If it does not pass validation the Name field text and border will turn red, error message generated
 
     function validName() {
         const inputName = $('#name');
@@ -154,7 +166,7 @@ $('#design').change(function(event){
         if (!(regexName.test($("#name").val()))) {
             
             inputName.css('border-color', '#B20000');
-            $('[for="name"] span').remove(); //
+            $('[for="name"] span').remove(); 
             $('[for="name"]').append('<span><b> Please type your name </b></span>').css('color', '#B20000');
             return false;
         } else {
@@ -167,12 +179,15 @@ $('#design').change(function(event){
 
     //Event handler listens for changes in Name field
     //Displays error message if validation rejected
+    //Displays error message if user clicks out of blank textbox (will engage before user submits form)
 
     $('#name').on('focusout', function () {
         validName();
     });
 
     //A validly formatted email address is required
+    //If it does not pass validation the Email field text and border will turn red, error message generated
+    //Displays tooltip in box if input is not valid (will engage before user submits form)
 
     function validEmail() {
         const regexEmail = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -190,29 +205,33 @@ $('#design').change(function(event){
         }
     }
 
-     //Event handler listens for changes in Email field
+    //Event handler listens for changes in Email field
     //Displays error message if a valid email is not provided
+    //Displays error message as user types, until valid format is supplied 
+    //Displays error message if user clicks out of blank textbox (will engage before user submits form)
 
-    $('#mail').on('focusout', function () {
+    $('#mail').on('focusout keyup', function () {
         validEmail();
     });
 
     
    //At least one item from Activities list must be selected
-
+   //If criteria not met the Activities text will turn red, error message generated
+   //If user deselects all activities, error message will occur until new selection is made (will engage before user submits form)
 
     function validActivities() {
         if($('input[type="checkbox"]').is(':checked')) {
             $('.activities legend span').remove();
+            $('.activities legend').css('color', '#000000');
             return true;
         } else {
             $('.activities legend span').remove();
-            $('.activities legend').append('<span>: Please select at least one activity from list </span>').css('color', '#B20000');
-        return false;
+            $('.activities legend').append('<span><b>: Please select at least one activity from list </b></span>').css('color', '#B20000');
+            return false;
         }
 
     }
-
+  
 
     //Event handler listens for changes in Activities list
     //Displays error message if validation rejected
@@ -223,6 +242,8 @@ $('#design').change(function(event){
 
 
     //CC requires >= 13 numbers, <= 16 numbers
+    //If validation not passed, CC text and border will turn red, error messages generated
+        //Seperate errors for: < 13 nums, > 16 nums, and input left blank
 
     function validCC() {
         const regexCC = /^(?:[0-9]{13,16})?$/;
@@ -230,7 +251,7 @@ $('#design').change(function(event){
         if ($('#cc-num').val().length < 13) {
             $('#cc-num').css('border-color', '#B20000');
             $('[for="cc-num"] span').remove();
-            $('[for="cc-num"]').append('<span><b> Credit card number should be at least 13 digits long </b></span>').css('color', '#B20000');
+            $('[for="cc-num"]').append('<span><b> Credit card number should be at least 13 digits long </span>').css('color', '#B20000');
             return false;
         } else if ($('#cc-num').val().length > 16) {
             $('#cc-num').css('border-color', '#B20000');
@@ -255,13 +276,14 @@ $('#design').change(function(event){
 
     //Event handler listens for changes in CC field
     //Displays error message if validation rejected
+    //Displays error message if user clicks out of blank textbox (will engage before user submits form)
 
     $('#cc-num').on('focusout', function () {
         validCC();
     });
 
-
     //Zip code requires 5 digits
+    //If validation not passed Zip text and border will turn red, error message generated
 
     function validZip() {
         const regexZip = /^\d{5}$/;
@@ -281,6 +303,7 @@ $('#design').change(function(event){
 
     //Event handler listens for changes is Zip code field
     //Displays error message if validation rejected
+    //Displays error message if user clicks out of blank textbox (will engage before user submits form)
 
     $('#zip').on('focusout', function () {
         validZip();
@@ -288,6 +311,7 @@ $('#design').change(function(event){
 
 
     //CVV requires 3 numbers for validation
+    //If validation not passed CVV text and border will turn red, error message generated
 
     function validCVV() {
         const regexCVV = /^[0-9]{3}$/;
@@ -306,14 +330,15 @@ $('#design').change(function(event){
     }
 
     //Event handler listens for changes in CVV field
-    //Displays error message if CVV is rejected
+    //Displays error message if CVV validation is rejected
+    //Displays error message if user clicks out of blank textbox (will engage before user submits form)
 
     $('#cvv').on('focusout', function () {
         validCVV();
     })
 
 
-    //Tests to ensure consitions for form validation on submit, preventing default behaviors
+    //Tests to ensure conditions for form validation on submit, preventing default behaviors
     //CC form fields only required when CC payment option is selected
 
   
